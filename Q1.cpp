5fd59f2deedb06ab1c310cf7c010ec700fb36257
AFAFWASILI <42271337+AFAFWASILI@users.noreply.github.com>
2026-04-05 08:38:05 +0300

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void particleInfo() {
+
+    // (a) Define the following variables:
+
+    // charge = -1
+    // mass = 0.000511
+    // momentum = 2.0
+
+
+
+    // (b) Print the particle information
+
+
+
+    // (c) Calculate the energy:
+    // E = sqrt(p^2 + m^2)
+
+
+
+    // (d) Print the energy
+
+
+
+    // (e) Using if / else:
+    // print "Charged particle" if charge != 0
+    // otherwise print "Neutral particle"
+}
